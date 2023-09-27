51f1e95111cd814d6902c8bc29bca500d930058d
Szymon Porwolik <szporwolik@gmail.com>
2023-09-27 02:40:56 +0200

    i18n

--- tools/tool_update_locales.py
+++ tools/tool_update_locales.py
@@ -17,8 +17,10 @@ with open(r'C:\Dev\sr0wx\tools\temp_files.txt', 'w') as f:
         f.write('\n')
 
 subprocess.call([r'C:\Program Files\gettext-iconv\bin\xgettext.exe',"-f",r'C:\Dev\sr0wx\tools\temp_files.txt',"-o",r'C:\Dev\sr0wx\locales\sr0wx.pot'])
+
+# PL
 subprocess.call([r'C:\Program Files\gettext-iconv\bin\msgmerge.exe',"--update",r'C:\Dev\sr0wx\locales\pl\LC_MESSAGES\sr0wx.po',r'C:\Dev\sr0wx\locales\sr0wx.pot'])
 subprocess.call([r'C:\Program Files\gettext-iconv\bin\msgfmt.exe',"-o",r'C:\Dev\sr0wx\locales\pl\LC_MESSAGES\sr0wx.mo',r'C:\Dev\sr0wx\locales\pl\LC_MESSAGES\sr0wx.po'])
 
-if os.path.exists(r'C:\Dev\sr0wx\tools\files.txt'):
-    os.remove(r'C:\Dev\sr0wx\tools\files.txt')
+if os.path.exists(r'C:\Dev\sr0wx\tools\temp_files.txt'):
+    os.remove(r'C:\Dev\sr0wx\tools\temp_files.txt')
